c7017ae6a4e3a6e850c717990f73bcc560c573f7
hotsno <71658949+hotsno@users.noreply.github.com>
2021-10-14 14:40:50 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,47 @@
+
+const CONFIG = {
+
+  // General
+  imageBackground: false,
+  openInNewTab: true,
+
+  // Links
+  cards: [
+    {
+      id: '1',
+      name: 'GitHub',
+      icon: 'github',
+      link: 'https://github.com/hotsno',
+    },
+    {
+      id: '2',
+      name: 'Steam',
+      icon: 'play-circle',
+      link: 'https://steamcommunity.com/id/hotsno/',
+    },
+    {
+      id: '5',
+      name: 'osu!',
+      icon: 'circle',
+      link: 'https://osu.ppy.sh/users/10076789',
+    },
+    {
+      id: '3',
+      name: 'AniList',
+      icon: 'list',
+      link: 'https://anilist.co/user/hotsno',
+    },
+    {
+      id: '4',
+      name: 'YouTube',
+      icon: 'youtube',
+      link: 'https://www.youtube.com/channel/UCFCulj7PEEsa3IL6f7RPUlw',
+    },
+    {
+      id: '6',
+      name: 'Twitch',
+      icon: 'twitch',
+      link: 'https://www.twitch.tv/hotsn0',
+    },
+  ]
+}
