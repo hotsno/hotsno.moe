108012782aa971d3a9066c9beae814def4c65099
hotsno <71658949+hotsno@users.noreply.github.com>
2023-07-10 01:00:27 -0700

    Go back to old site

---
+++ assets/js/cards.js
@@ -0,0 +1,24 @@
+const printCards = () => {
+  for (const card of CONFIG.cards) {
+
+    let item = `
+        <a
+          href="${card.link}"
+          target="${CONFIG.openInNewTab ? "_blank" : ""}"
+          class="buttonLink__link card buttonLink__link-${
+            card.id
+          }"
+        >
+          <i class="buttonLink__icon" data-feather="${
+            card.icon
+          }"></i>
+        </a>
+    `;
+
+    const position = "beforeend";
+
+    buttonsContainer.insertAdjacentHTML(position, item);
+  }
+};
+
+printCards();
